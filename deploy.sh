432a68741c427763787bf70b0def463d5c61b8f8
Michal <michalldbs@seznam.cz>
2023-02-20 20:50:39 +0100

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+REPO_PATH="/home/michal/educa-sis"
+
+if [[ ! -d "${REPO_PATH}" ]]; then
+  git clone https://github.com/mdobes/educa-sis "${REPO_PATH}"
+fi
+
+sudo docker exec educa-sis php artisan down --secret="secret-key"
+
+cd "${REPO_PATH}" &&
+  git clean -fd &&
+  git checkout -- . &&
+  git checkout main &&
+  git reset --hard origin/main &&
+  git pull
+
+sudo docker-compose -f ${REPO_PATH}/docker-compose.yml up --build -d
+
+sudo docker exec -e COMPOSER_MEMORY_LIMIT=-1 web-educasis composer install --no-scripts
+sudo docker exec web-educasis composer dump-autoload --optimize --classmap-authoritative
+sudo docker exec web-educasis php artisan cache:clear
+sudo docker exec web-educasis php artisan route:clear
+sudo docker exec web-educasis php artisan view:clear
+sudo docker exec web-educasis php artisan migrate --force
+sudo docker exec web-educasis php artisan ldap:import
+
+sudo chmod -R 0777 ${REPO_PATH}
