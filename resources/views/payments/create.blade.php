970899d2d8685bb478688a5abad50f58d60fc198
Michal <michalldbs@seznam.cz>
2023-02-27 12:18:22 +0100

    úpravy práv

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -21,50 +21,52 @@
             {!! Form::label("text", "Plátce", ["class" => "form-label"]) !!}
             {!! Form::select("payer[]", [], null, ["id" => "payer", "class" => "form-control", "required" => true, "multiple" => true, "data-select2-enable" => true]) !!}
         </div>
+        @if(Auth::user()->permission == "admin")
         <div class="mb-3">
             {!! Form::label("text", "Typ", ["class" => "form-label"]) !!}
             {!! Form::select("type", ["normal" => "Klasická", "adobe" => "Automatické přiřazení Adobe CC účtu"], null, ["class" => "form-select", "required" => true]) !!}
         </div>
-        <div class="mb-3">
-            {!! Form::label("text", "Splatnost", ["class" => "form-label"]) !!}
-            {!! Form::input("date", "due", $data->due ?? null, ["class" => "form-control", "required" => true]) !!}
-        </div>
-        <div class="mb-3">
-            {!! Form::button($formButtonTitle, ["type" => "submit", "class" => "btn btn-primary"]) !!}
-        </div>
-
+        @endif
+    <div class="mb-3">
+        {!! Form::label("text", "Splatnost", ["class" => "form-label"]) !!}
+        {!! Form::input("date", "due", $data->due ?? null, ["class" => "form-control", "required" => true]) !!}
     </div>
-    {!! Form::close() !!}
+    <div class="mb-3">
+        {!! Form::button($formButtonTitle, ["type" => "submit", "class" => "btn btn-primary"]) !!}
+    </div>
+
+</div>
+{!! Form::close() !!}
 @endsection
 
 @section("scripts")
-    <script>
-        $('#payer').select2({
-            theme: 'bootstrap-5',
-            language: "cs",
-            minimumInputLength: 3,
-            ajax: {
-                url: "{{route("payment.searchpayers")}}",
-                data: function (params) {
-                    return {
-                        search: params.term,
-                    };
-                },
-                processResults: function (data) {
-                    return {
-                        results: $.map(data.users, function (obj) {
-                            obj.id = "user:" + obj.typeId;
+<script>
+    $('#payer').select2({
+        theme: 'bootstrap-5',
+        language: "cs",
+        minimumInputLength: 3,
+        ajax: {
+            url: "{{route("payment.searchpayers")}}",
+            data: function (params) {
+                return {
+                    search: params.term,
+                };
+            },
+            processResults: function (data) {
+                return {
+                    results: $.map(data.users, function (obj) {
+                        obj.id = "user:" + obj.typeId;
+                        return obj;
+                    }).concat(
+                        $.map(data.groups, function (obj) {
+                            obj.id = "group:" + obj.typeId;
                             return obj;
-                        }).concat(
-                            $.map(data.groups, function (obj) {
-                                obj.id = "group:" + obj.typeId;
-                                return obj;
-                            })
-                        )
-                    };
-                }
+                        })
+                    )
+                };
             }
-        });
+        }
+    });
 
-    </script>
+</script>
 @endsection
